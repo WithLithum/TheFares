cf4e77ef4e8dee538dec7c537cb1541cbf1ab5ab
RelaperCrystal <RelaperCrystal@163.com>
2021-05-15 09:25:14 +0800

    🚧 Entry point definition

--- TheFares/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TheFares
-{
-    public class Class1
-    {
-    }
-}
---
+++ TheFares/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rage.Attributes;
+
+[assembly: Plugin("The Fares", Author = "RelaperCrystal", SupportUrl = "https://github.com/RelaperCrystal/TheFares/issues", Description = "The fares plugin", PrefersSingleInstance = true)]
+
+namespace TheFares
+{
+    internal static class EntryPoint
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
